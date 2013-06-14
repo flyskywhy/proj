1166dc7de367d9fc3bd1a41efbd4c2550d5648e5
Li Zheng <flyskywhy@gmail.com>
2013-06-14 17:08:46 +0800

    env.sh: Xilinx; proj: mbref, lwip_app_base, microblazeel-unknown-linux-gnu linux

--- env.sh
+++ env.sh
@@ -4,12 +4,22 @@ PROJ_ROOT=`pwd`
 export PROJ_ROOT
 
 PATH=$PROJ_ROOT/proj:$PATH
-PATH=$PROJ_ROOT/tools/eclipse:$PATH
+PATH=$PROJ_ROOT/ecos/scripts:$PATH
+PATH=$PROJ_ROOT/ecos/examples:$PATH
 PATH=$PROJ_ROOT/tools/ecos-tools/bin:$PATH
-PATH=$PROJ_ROOT/tools/mipsisa32-elf/bin:$PATH
+PATH=$PROJ_ROOT/tools/microblazeel-unknown-linux-gnu/bin:$PATH
 export PATH
 
 ECOS_REPOSITORY=$PROJ_ROOT/ecos/packages
 export ECOS_REPOSITORY
+
+ECOS_BSP="ecos_v2_00_a"
+export ECOS_BSP
+
+# On all modern variants of Windows (including Cygwin and Wine)
+# the OS environment variable is defined to 'Windows_NT'
+if [ ! 777${OS} = 777"Windows_NT" ]; then
+. /opt/Xilinx/13.1/ISE_DS/settings32.sh
+fi
 
 echo Your PROJ root is set to \'$PROJ_ROOT\'
